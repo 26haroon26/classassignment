69f0d4cf8a5369fa62f02ca6849cfba86d9dd553
26haroon26 <26haroon26@gmail.com>
2022-10-19 07:42:51 -0700

    'abc'

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const[num,setnum] = useState(1);
- 
+  const [num, setnum] = useState(0);
+  const [mode, setmode] = useState(true);
+  const changemode = () => {
+    setmode(!mode);
+  };
   return (
-    <div className='center'>
-   <h1 className='counter'>Counter {num}</h1>
-   <button className='add' onClick={()=>{
-    setnum(num + 1);
-   }}> Add</button>
-   <button className='minus' onClick={()=>{
-    setnum(num - 1);
-   }}>Minus</button>
-    </div>
+    <>
+      <div className={`body ${mode ? "soft" : "hard"} `}>
+        <div className="Mode">
+          <img
+            onClick={changemode}
+            className={`${mode ? "white" : "black"} `}
+            id="sunmoon"
+            src={`./images/${mode ? "moon" : "sun"}.png`}
+            alt=""
+          />
+        </div>
+        <div className={`center ${mode ? "white" : "black"} `}>
+          <h1 className={`counter ${mode ? "light" : "dark"} `}>
+            Counter {num}
+          </h1>
+          <button
+            className={`add ${mode ? "slow white" : "fast black"} `}
+            onClick={() => {
+              setnum(num + 1);
+            }}
+          >
+            {" "}
+            Add
+          </button>
+          <button
+            className={`minus ${mode ? "slow white" : "fast black"} `}
+            onClick={() => {
+              setnum(num - 1);
+            }}
+          >
+            Sub
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
